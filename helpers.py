8e0f8b1ec4dcf0f47ce81d9ad9914ec065b39f07
Pedro Crespan <pedrocrespan@hotmail.com>
2022-12-27 16:24:06 -0300

    Used nargs to fix args number bug

--- helpers.py
+++ helpers.py
@@ -2,12 +2,15 @@ import argparse
 import csv
 
 
-parser = argparse.ArgumentParser(prog="todoTUI", description="TUI to-do list")
-parser.add_argument("-a", help="Add task")
-args = parser.parse_args()
-
-
 def addTask(task, description):
-    with open("todoTasks.csv") as file:
+    with open("todoTasks.csv", "w") as file:
         writer = csv.DictWriter(file, fieldnames=["task", "description"])
         writer.writerow({"task": task, "description": description})
+
+
+parser = argparse.ArgumentParser(prog="todoTUI", description="TUI to-do list")
+parser.add_argument("-a", nargs="+", help="Add task")
+args = parser.parse_args()
+
+if args.a:
+    addTask(args.a[0], args.a[1])
